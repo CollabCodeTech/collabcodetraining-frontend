1c071eeda2d2246c24a7d21320c4a6e3c4322d93
Demys Lima <demys.lima@contabilizei.com.br>
2019-10-08 10:30:45 -0300

    menu mobile e prévia das páginas

--- src/containers/MenuMobileCollab.svelte
+++ src/containers/MenuMobileCollab.svelte
@@ -5,17 +5,41 @@
 </script>
 
 <style>
+  .menu-mobile {
+    background: var(--color-arsenic);
+    border-top: 2px solid var(--color-pomp-and-power);
+  }
 
+  .menu-mobile > .menu-list {
+    display: flex;
+    justify-content: space-between;
+    padding: var(--gap-normal);
+  }
+
+  .menu-mobile > .menu-list > .menu-option > a {
+    display: flex;
+    flex-direction: column;
+    text-decoration: none;
+    color: white;
+  }
+
+  .menu-mobile > .menu-list > .menu-option > :global(a.selected) {
+    color: var(--color-fiery-rose);
+  }
 </style>
 
-<nav>
-  <ul>
+<nav class="menu-mobile">
+  <ul class="menu-list">
     {#each menu as option}
-      <li>
+      <li class="menu-option">
         <a
-          class={segment === undefined ? 'selected' : ''}
-          href={option.toLowerCase()}>
-          <IconCollab alt={option.toLowerCase()} />
+          href={option.toLowerCase()}
+          class={segment === option.toLowerCase() ? 'selected' : ''}>
+          
+            <IconCollab
+              alt={`Página de ${option.toLowerCase()}`}
+              name={option && option.toLowerCase() === segment ? `${option.toLowerCase()}-selected`: option.toLowerCase()} />
+        
           {option}
         </a>
       </li>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,6 @@
 <script>
   import HeaderCollab from '../containers/HeaderCollab.svelte'
-  import MenuMobileCollab from '../containers/MenuMobileCollab'
+  import MenuMobileCollab from '../containers/MenuMobileCollab.svelte'
 
   export let segment
 
@@ -8,6 +8,12 @@
 </script>
 
 <style>
+
+:global(.menu-mobile) {
+  position: fixed;
+  bottom: 0;
+  width: 100vw;
+}
 
 </style>
 
