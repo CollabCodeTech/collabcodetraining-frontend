9ff67d94fad07ad8db8451c4eb15546fc8f9dfa2
Felipe Marcon <felipeemarcon@gmail.com>
2019-10-16 08:44:54 -0300

    Create button component

--- src/routes/signup.svelte
+++ src/routes/signup.svelte
@@ -1,6 +1,7 @@
 <script>
   import LogoCollab from '../components/LogoCollab.svelte'
   import TitleCollab from '../components/TitleCollab.svelte'
+  import ButtonCollab from '../components/ButtonCollab.svelte'
 </script>
 
 <style>
@@ -10,3 +11,4 @@
 <LogoCollab />
 
 <TitleCollab content="Cadastro" uppercase />
+<ButtonCollab label="Enviar" />
