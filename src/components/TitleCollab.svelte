bdfaf7b9dbbb47a1175223510f835e604352d9cd
Marco Bruno <marco.bruno.br@gmail.com>
2019-10-16 03:14:05 +0100

    :sparkles: Add the variation -uppercase

--- src/components/TitleCollab.svelte
+++ src/components/TitleCollab.svelte
@@ -1,11 +1,19 @@
 <script>
-  export let text
+  export let content = ''
+  export let uppercase = false
 </script>
 
 <style>
   .title-collab {
     color: var(--color-floral-white);
   }
+
+  .title-collab.-uppercase {
+    text-transform: uppercase;
+    font-size: var(--size-small);
+    line-height: 1.5;
+    letter-spacing: 3px;
+  }
 </style>
 
-<h6 class="title-collab">{text}</h6>
+<h6 class={`title-collab ${uppercase && '-uppercase'}`}>{content}</h6>
