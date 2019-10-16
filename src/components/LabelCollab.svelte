697205370f9bb04aa5d4972ece8a3c37bbb0f0b9
Helton Fábio <heltonfabio@outlook.com>
2019-10-16 20:07:16 -0300

    removing commentaries

--- src/components/LabelCollab.svelte
+++ src/components/LabelCollab.svelte
@@ -5,14 +5,12 @@
 
 <style>
   .label-collab {
-    /* size */
     display: inline-block;
     width: var(--size-biggest);
     height: var(--size-normal);
     margin-right: var(--gap-small);
     box-sizing: border-box;
 
-    /* font configuration */
     font-family: inherit;
     line-height: var(--size-normal);
     font-size: var(--size-subtitle-first);
@@ -20,7 +18,6 @@
     text-align: center;
     color: var(--color-floral-white);
 
-    /* box configuration */
     background-color: var(--color-fiery-rose);
     border-radius: var(--size-normal);
   }
