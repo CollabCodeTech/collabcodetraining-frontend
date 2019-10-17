9c9d569370a97bb0de41e1132ab1456dc73f254a
Marco Bruno <marco.bruno.br@gmail.com>
2019-10-17 01:20:19 +0100

    :construction: create base of the component InputCollab

---
+++ src/components/InputCollab.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let type = 'text'
+  export let name = 'No name'
+  export let placeholder = 'No placeholder'
+</script>
+
+<style>
+  .input-collab {
+    color: var(--color-fiery-rose);
+    font-size: var(--size-smallest);
+    letter-spacing: 0.15px;
+    background-color: transparent;
+    padding-bottom: var(--gap-normal);
+    border-bottom: 1px solid var(--color-floral-white);
+  }
+</style>
+
+<input class="input-collab" {type} {name} {placeholder} />
--- src/routes/signup.svelte
+++ src/routes/signup.svelte
@@ -3,6 +3,7 @@
   import TitleCollab from '../components/TitleCollab.svelte'
   import ButtonCollab from '../components/ButtonCollab.svelte'
   import LabelCollab from '../components/LabelCollab.svelte'
+  import InputCollab from '../components/InputCollab.svelte'
 </script>
 
 <style>
@@ -14,5 +15,12 @@
 <TitleCollab content="Cadastro" uppercase />
 
 <LabelCollab content="Nome:" />
+<InputCollab type="text" name="name" placeholder="Seu nome" />
+
+<LabelCollab content="Email:" />
+<InputCollab type="email" name="email" placeholder="example@gmail.com" />
+
+<LabelCollab content="Senha:" />
+<InputCollab type="password" name="password" placeholder="********" />
 
 <ButtonCollab label="Enviar" />
