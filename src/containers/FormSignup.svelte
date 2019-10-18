f682fc89c37971fcd10364d4407aa026098eb7e4
Marco Bruno <marco.bruno.br@gmail.com>
2019-10-18 01:04:10 +0100

    :sparkles: create the FormSignup

---
+++ src/containers/FormSignup.svelte
@@ -0,0 +1,43 @@
+<script>
+  import FieldCollab from '../components/FieldCollab.svelte'
+  import ButtonCollab from '../components/ButtonCollab.svelte'
+</script>
+
+<style>
+  .form-signup {
+    text-align: left;
+  }
+
+  .form-signup > :global(.field-collab) {
+    margin-bottom: var(--gap-big);
+  }
+
+  .form-signup :global(.input-collab) {
+    margin-bottom: var(--gap-smallest);
+  }
+</style>
+
+<form action="" class="form-signup">
+  <FieldCollab
+    content="Nome:"
+    id="name"
+    type="text"
+    name="name"
+    placeholder="Seu nome" />
+
+  <FieldCollab
+    content="Email:"
+    id="email"
+    type="email"
+    name="email"
+    placeholder="example@gmail.com" />
+
+  <FieldCollab
+    content="Senha:"
+    id="password"
+    type="password"
+    name="password"
+    placeholder="********" />
+
+  <ButtonCollab content="Enviar" />
+</form>
