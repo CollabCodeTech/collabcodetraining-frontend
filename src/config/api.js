be9c2fe7b1c7f68c8ad10784a1822880b86fe49e
Marco Bruno <marco.bruno.br@gmail.com>
2019-11-09 01:24:03 +0000

    :construction: Add withCredentials: true

--- src/config/api.js
+++ src/config/api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'http://localhost:5001/api',
+  withCredentials: true,
 })
 
 api.interceptors.response.use(response => response, error => error.response)
